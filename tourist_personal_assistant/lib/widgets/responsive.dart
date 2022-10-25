9215e37f4086400ab342c24ac281162e183a0063
KNkoe <katleholnkoe@gmail.com>
2022-10-25 18:25:36 +0200

    intitial commit

---
+++ tourist_personal_assistant/lib/widgets/responsive.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Size screenSize(BuildContext context) {
+  return MediaQuery.of(context).size;
+}
---
+++ tourist_personal_assistant/lib/widgets/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+    scaffoldBackgroundColor: const Color(0xffF5F5F5),
+    primaryColor: const Color(0xff0089EF),
+    appBarTheme: const AppBarTheme(
+        titleTextStyle: TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18)));
